20da00a719ad9d5401f9f064e9b62f17c0de5747
sonusathyadas <sonusathyadas@hotmail.com>
2026-03-02 20:19:03 +0530

    Implement CORS policy and Swagger support in development environment

--- BookApi/Program.cs
+++ BookApi/Program.cs
@@ -3,8 +3,25 @@ using BookApi.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+// Add Swagger only in development
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+}
 
-// Add services to the container.
+
+// Add CORS policy - restrict to trusted origins
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("TrustedOrigins", policy =>
+    {
+        policy.WithOrigins("https://yourtrustedclient.com") // TODO: Replace with actual trusted origins
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -13,6 +30,23 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddScoped<IBookRepository, BookRepository>();
 
 var app = builder.Build();
+// Use Swagger only in development
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Global error handler to avoid stack trace exposure
+app.UseExceptionHandler(errorApp =>
+{
+    errorApp.Run(async context =>
+    {
+        context.Response.StatusCode = 500;
+        context.Response.ContentType = "application/json";
+        await context.Response.WriteAsync("{\"message\":\"An unexpected error occurred.\"}");
+    });
+});
 
 // Ensure the database is created.
 using (var scope = app.Services.CreateScope())
@@ -23,6 +57,9 @@ using (var scope = app.Services.CreateScope())
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
+
+// Use CORS policy
+app.UseCors("TrustedOrigins");
 
 app.MapControllers();
 
