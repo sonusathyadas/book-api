7f5b3f6fe76529e73e15294e927fa6aa4ab12b54
sonusathyadas <sonusathyadas@hotmail.com>
2026-03-02 19:43:14 +0530

    Add Swagger support and Lua settings for development

--- BookApi/Program.cs
+++ BookApi/Program.cs
@@ -1,6 +1,7 @@
 using BookApi.Data;
 using BookApi.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,8 +22,23 @@ using (var scope = app.Services.CreateScope())
     db.Database.EnsureCreated();
 }
 
+builder.Services.AddEndpointsApiExplorer(); // Required for Minimal APIs, but good practice for controllers too
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
+
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
+
+// if (app.Environment.IsDevelopment()) // Often enabled only in Development environments for security
+// {
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
+// }
 
 app.MapControllers();
 
